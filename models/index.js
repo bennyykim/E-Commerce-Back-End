3410ef4228536e4025e2e491a70e4dc8371ff210
Benjamin Kim <bennykim523@gmail.com>
2022-02-01 19:02:38 -0500

    added tables

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
